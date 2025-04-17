18e74c9a1ba70cc9841c4b0e21d6b6ffee0caa28
yan3ku <wisniewskijas@outlook.com>
2025-04-17 18:26:52 +0200

    NaN box everything

--- types/sexp.h
+++ types/sexp.h
@@ -1,86 +1,180 @@
-/* todo add support for macros / builtin tokens */
 /*
 #include <stdlib.h>
 #include <assert.h>
 #include <stddef.h>
 #include <stdalign.h>
 #include "types/arena.h"
-*/
 
-#define nil NULL
+ *
+ * NaN is 12 bits (11 exponent and 1 mantysa MSB) set to 1
+ * QNAN is 13
+ * DOUBLE is any not QNAN value
+ * POINTER has 48 bits of payload; there is 64 - 13 - 48 = 3 left for tag
+ * one of these is used to tag other types that don't use pointer payload
+ *
+ * | NOT QNAN       | DOUBLE
+ * | 0[N]00........ | CELL
+ * | 0[N]01........ | SYMBOL
+ * | 0[N]10........ | STRING
+ * | 0[N]11........ | VECTOR
+ * | 1[N]00........ | TABLE
+ * | 1[N]01........ | FUNCTION
+ * | 1[N]10........ | (reserved)
+ *
+ * | 1[N]11|00..... | INTEGER
+ * | 1[N]11|01..... | NIL (empty list)
+ * | 1[N]11|10...XX | CHARACTER
+ * | 1[N]11|11...XX | METADATA
+ * | 1[N]11|11....X | BOOLEAN
+ *              ..1 - T
+ *              ..0 - F
+ */
 
-/* atom cells don't have special mask so they can be dereferenced normaly */
-#define CELL_MASK (1UL << 63)
-#define A_CONS    CELL_MASK
-#define A_ATOM    0
 
-/* probably good idea to clear tag? it will break anyway idk about that */
-#define CELL_CL_TAG(p) ((uint64_t)(p) & ~CELL_MASK)
-#define CELL(p)        ((Cell*)((uint64_t)(p) & 0xFFFFFFFFFFFF))
-#define CONSP(p)       (p && ((uint64_t)p & CELL_MASK) == A_CONS)
-#define ATOMP(p)       (p && ((uint64_t)p & CELL_MASK) == A_ATOM)
-#define TO_CONS(p)     ((void*)(CELL_CL_TAG(p) | A_CONS))
-#define TO_ATOM(p)     ((void*)(CELL_CL_TAG(p) | A_ATOM))
-#define CELL_AT(p)     (CELL(p)->loc_.at) /* cell location in file */
-#define CELL_LEN(p)    (CELL(p)->loc_.len) /* cell lenght in file */
-#define CELL_LOC(p)    (CELL(p)->loc_)
-#define CAR(p)         (CELL(p)->car_)
-#define CDR(p)         (CELL(p)->cdr_)
+#include <stdint.h>
+#define QNAN	      0x7ffc000000000000
+#define PTR_TAG_MASK  0xffff000000000000
+#define CEL_MASK      0x7ffc000000000000
+#define SYM_MASK      0x7ffd000000000000
+#define STR_MASK      0x7ffe000000000000
+#define VEC_MASK      0x7fff000000000000
+#define TAB_MASK      0xfffc000000000000
+#define FUN_MASK      0xfffd000000000000
+/* RESERVED           0xfffe000000000000 */
 
-#define RANGEFMT "<%lu,%lu>"
+#define NPTR_TAG_MASK 0xfffff00000000000
+#define INT_MASK      0xffff000000000000
+#define MET_MASK      0xffff000000000000
+#define NIL_MASK      0xffff100000000000
+#define BOL_MASK      0xffff200000000000
+#define T_MASK        0xffff200000000001
+#define F_MASK        0xffff200000000000
+
+/* predicates */
+#define DUBP(v) ((v.as_uint64 & QNAN) != QNAN)
+#define CELP(v) ((v.as_uint64 &  PTR_TAG_MASK) == CEL_MASK)
+#define SYMP(v) ((v.as_uint64 &  PTR_TAG_MASK) == SYM_MASK)
+#define STRP(v) ((v.as_uint64 &  PTR_TAG_MASK) == STR_MASK)
+#define VECP(v) ((v.as_uint64 &  PTR_TAG_MASK) == VEC_MASK)
+#define TABP(v) ((v.as_uint64 &  PTR_TAG_MASK) == TAB_MASK)
+#define FUNP(v) ((v.as_uint64 &  PTR_TAG_MASK) == FUN_MASK)
+#define INTP(v) ((v.as_uint64 & NPTR_TAG_MASK) == INT_MASK)
+#define BOLP(v) ((v.as_uint64 & NPTR_TAG_MASK) == BOL_MASK)
+#define METP(v) ((v.as_uint64 & NPTR_TAG_MASK) == MET_MASK)
+#define NUMP(v) (INTP(v) || DUBP(v))
+#define NILP(v) (v.as_uint64 == NIL_MASK)
+#define ATMP(v) (!CELP(v))
+
+/* get value */
+#define AS_PTR(v) ((char*)((v).as_uint64 & 0xFFFFFFFFFFFF))
+#define AS_CEL(v) ((Cell*)AS_PTR(v))
+#define AS_SYM(v) ((char*)AS_PTR(v))
+#define AS_STR(v) ((char*)AS_PTR(v))
+#define AS_VEC(v) ((Vec*) AS_PTR(v))
+#define AS_TAB(v) ((ht*)  AS_PTR(v))
+#define AS_FUN(v) ((fn *) AS_PTR(v))
+
+#define AS_DUB(v) (v.as_double)
+#define AS_BOL(v) (v.as_byte)
+#define AS_INT(v) (v.as_uint32)
+#define AS_MET(v) (AS_INT(v))
+
+#define NIL       ((Value){.as_uint64 = NIL_MASK})
+#define T         ((Value){.as_uint64 = T_MASK})
+#define F         ((Value){.as_uint64 = F_MASK})
+
+/* cell */
+#define CAR(p)    (AS_CEL(p)->car_)
+#define CDR(p)    (AS_CEL(p)->cdr_)
+
+/* add tag mask */
+#define TO_DUB(d) ((Value){.as_double = d})
+#define TO_CEL(p) ((Value){.as_uint64 = (uint64_t)(p) | CEL_MASK})
+#define TO_SYM(p) ((Value){.as_uint64 = (uint64_t)(p) | SYM_MASK})
+#define TO_STR(p) ((Value){.as_uint64 = (uint64_t)(p) | STR_MASK})
+#define TO_VEC(p) ((Value){.as_uint64 = (uint64_t)(p) | VEC_MASK})
+#define TO_TAB(p) ((Value){.as_uint64 = (uint64_t)(p) | TAB_MASK})
+#define TO_FUN(p) ((Value){.as_uint64 = (uint64_t)(p) | FUN_MASK})
+#define TO_MET(p) ((Value){.as_uint64 = (uint64_t)(p) | MET_MASK})
+#define TO_INT(i) ((Value){.as_uint64 = (uint64_t)(uint32_t)(i) | INT_MASK}) /* zero-extend negative */
+
+#define TYPE_ERR(type, val)						\
+	printf("; The value\n;\t%s\n; doesn't satisfy predicate\n;\t%s\n", valuestr(val), type)
+
+#define AS_NUM(val) (INTP(val) ? AS_INT(val) :		\
+		     DOUBLP(val) ? AS_DOUBL(val) :	\
+		     (assert(0 && "unreachable"), 0))
+
+#define ASSERTV(type, val) (!type(val) ? (TYPE_ERR(#type, val), 1) : 0)
+
+
+#define RANGEFMT "|%lu:%lu|"
 #define RANGEP(range) range.at, range.len
-
-typedef enum {
-	A_SYM,
-	A_STR,
-	A_INT,
-	A_DOUBL,
-	A_VEC,
-} AtomVar;
 
 typedef struct {
 	size_t at;
 	size_t len;
 } Range;
 
-typedef struct Cell {
-	Range loc_;
-	union {
-		struct {
-			struct Cell *car_; /* don't use these directly */
-			struct Cell *cdr_; /* they have upper bits set to type */
-		};
-		struct {
-			AtomVar type;
-			union {	/* literals */
-				struct Cell *vec;
-				char *string;
-				double doubl;
-				int integer;
-			};
-		};
-	};
-} Cell;				/* 32 bits */
+typedef union {
+	uint8_t  as_byte;
+	uint32_t as_uint32;
+	uint64_t as_uint64;
+	double   as_double;
+} Value;
+
+typedef struct  {
+	Value car_;
+	Value cdr_;
+} Cell;
 
 typedef struct {
 	const char *fname;
 	Arena *arena;
-	Cell *cell;
+	Value cell;
 } Sexp;
 
-static inline Cell *
-cellof(Arena *arena, uint64_t mask, size_t at) {
-	Cell *cell = (Cell *)(CELL_CL_TAG(new(arena, sizeof(Cell))) | mask);
-	CELL_AT(cell) = at;
-	CELL_LEN(cell) = SIZE_MAX;
-	return cell;
-}
-
-static inline Cell *
-cons(Arena *arena, Cell *a, Cell *b, size_t at)
+static inline Value
+cons(Arena *arena, Value a, Value b, size_t at)
 {
-	Cell *cell = cellof(arena, A_CONS, at);
+	Value cell = TO_CEL(new(arena, at));
 	CAR(cell) = a;
 	CDR(cell) = b;
 	return cell;
 }
+
+static inline const char *
+valuestr(Value val)
+{
+	static char buff[BUFSIZ];
+	if      (INTP(val)) snprintf(buff, BUFSIZ, "%d", AS_INT(val));
+	else if (DUBP(val)) snprintf(buff, BUFSIZ, "%f", AS_DUB(val));
+	else if (SYMP(val)) snprintf(buff, BUFSIZ, "%s", AS_PTR(val));
+	else if (BOLP(val)) snprintf(buff, BUFSIZ, "%s", AS_BOL(val) ? "T" : "F");
+	else if (CELP(val)) snprintf(buff, BUFSIZ, "CELL");
+	else if (NILP(val)) snprintf(buff, BUFSIZ, "nil");
+	else assert(0 && "valuestr: invalid type; unreachable");
+	return buff;
+}
+
+inline static void
+printes_(Value cell) {
+	if (CELP(cell)) {
+		putchar('(');
+		printes_(CAR(cell));
+		while (CELP(CDR(cell)))  {
+			putchar(' ');
+			cell = CDR(cell);
+			printes_(CAR(cell));
+		};
+		if (ATMP(CDR(cell)) && !NILP(CDR(cell))) {
+			fputs(" . ", stdout);
+			printes_(CDR(cell));
+		}
+		putchar(')');
+		return;
+	}
+	printf("%s", valuestr(cell));
+}
+
+void printes(Sexp *sexp) { printes_(sexp->cell); }
